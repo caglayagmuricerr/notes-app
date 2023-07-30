d2b5206fdb7f009eaf1a15a26742fc2239ef8f92
caglayagmuricerr <119360644+caglayagmuricerr@users.noreply.github.com>
2023-07-30 12:09:33 +0300

    googlefonts doesn't work

---
+++ lib/fonts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle customFontStyle = GoogleFonts.dancingScript(
+  fontSize: 16.0, // You can adjust the font size as needed
+  fontWeight: FontWeight.normal, // You can also change the font weight if required
+  fontStyle: FontStyle.normal, // You can set the font style (normal, italic, or oblique)
+  letterSpacing: 1.0, // You can set the letter spacing as well
+);
